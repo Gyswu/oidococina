1ac222c1813ffd453bbd86e9274f978fb9868bec
Gyswu <o.lugovskyy@gmail.com>
2019-05-29 21:21:17 +0200

    Copia de seguridad

--- app/AdminModule/Presenters/MesasPresenter.php
+++ app/AdminModule/Presenters/MesasPresenter.php
@@ -2,108 +2,83 @@
 
 namespace App\AdminModule\Presenters;
 
-
 use App\Forms\FormFactory;
 use App\Forms\MesasFormFactory;
 use App\Model\Orm\Mesa;
 use Nette\Application\UI\Form;
-use Nette\ComponentModel\IComponent;
-use Nette\Database\Table\ActiveRow;
 
-class MesasPresenter extends BaseAdminPresenter
-{
+class MesasPresenter extends BaseAdminPresenter {
+    
     /** @var $mesaEditada Mesa */
     private $mesaEditada;
-
-    public function renderDefault(): void
-    {
+    
+    public function renderDefault(): void {
         $this->template->mesas = $this->orm->mesas->findAll();
     }
-
-
-    public function createComponentMasMesasForm()
-    {
-
-        $form = (new FormFactory())->create();
-
+    
+    public function createComponentMasMesasForm() {
+        
+        $form = ( new FormFactory() )->create();
         $form->addText('nombre', 'Nombre de la mesa')
-            ->setRequired();
-
+             ->setRequired();
         $form->addSubmit('send', 'Añadir')
-            ->setHtmlAttribute("class", 'btn btn-success');
-
-        $form->onSuccess[] = [$this, 'onSuccessMasMesas'];//convención con la variable onSuccess y el nombre del formulario
-
+             ->setHtmlAttribute("class", 'btn btn-success');
+        $form->onSuccess[] = [ $this, 'onSuccessMasMesas' ];//convención con la variable onSuccess y el nombre del formulario
+        
         return $form;
-
     }
-
-
-    public function onSuccessMasMesas(Form $form, \stdClass $values): void
-    {
-
+    
+    public function onSuccessMasMesas( Form $form, \stdClass $values ): void {
+        
         $mesa = new Mesa();
         $mesa->nombre = $values->nombre;
-
-
         try {
             $mesaNueva = $this->orm->mesas->persistAndFlush($mesa);
             $this->flashMessage('La mesa ha sido añadido a la base de datos', 'success');
-        } catch (\Exception $e) {
-            $this->flashMessage("Error: ".$e->getMessage(), 'danger');
+        } catch( \Exception $e ) {
+            $this->flashMessage("Error: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('this');
-
     }
-
-    public function actionEditar($id)
-    {
+    
+    public function actionEditar( $id ) {
         $mesa = $this->orm->mesas->getById($id);
         $this->mesaEditada = $mesa;
-
         $this->template->item = $this->orm->mesas->findById($id);
     }
-
-    public function createComponentEditarMesaForm()
-    {
-
-        $form = (new MesasFormFactory())->createEdit($this->mesaEditada);
-
-        $form->onSuccess[] = [$this, 'onSuccessEditarMesa'];//convención con la variable onSuccess y el nombre del formulario
-
+    
+    public function createComponentEditarMesaForm() {
+        
+        $form = ( new MesasFormFactory() )->createEdit($this->mesaEditada);
+        $form->onSuccess[] = [ $this, 'onSuccessEditarMesa' ];//convención con la variable onSuccess y el nombre del formulario
+        
         return $form;
     }
-
-    public function onSuccessEditarMesa(Form $form, \stdClass $values)
-    {
-
+    
+    public function onSuccessEditarMesa( Form $form, \stdClass $values ) {
+        
         try {
             $mesa = $this->mesaEditada;
             $mesa->nombre = $values->nombre;
-
             $this->orm->mesas->persistAndFlush($mesa);
             $this->flashMessage("Mesa editada correctamente", "success");
-        } catch (\Exception $e) {
+        } catch( \Exception $e ) {
             $this->flashMessage("Error al editar", 'danger');
         }
-
         $this->redirect("this");
     }
-
-    public function actionBorrar($id)
-    {
+    
+    public function actionBorrar( $id ) {
         try {
-            if (!$plato = $this->orm->mesas->findById($id)) {
+            if( !$plato = $this->orm->mesas->findById($id) ) {
                 $this->flashMessage("La mesa no existe", "danger");
             };
             $this->orm->mesas->remove($id);
             $this->flashMessage("Mesa eliminado", "success");
-        } catch (\Exception $e) {
+        } catch( \Exception $e ) {
             $this->flashMessage("Error al eliminar la mesa, ¿Es una id válida?: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('default');
     }
-
+    
 }
--- app/AdminModule/Presenters/PlatosPresenter.php
+++ app/AdminModule/Presenters/PlatosPresenter.php
@@ -2,52 +2,40 @@
 
 namespace App\AdminModule\Presenters;
 
-
 use App\Forms\FormFactory;
 use App\Forms\PlatosFormFactory;
-use App\Model\Orm\Plato;
 use App\Model\Orm\Ingrediente;
+use App\Model\Orm\Plato;
 use Nette\Application\UI\Form;
-use Nette\ComponentModel\IComponent;
-use Nette\Database\Table\ActiveRow;
 
-class PlatosPresenter extends BaseAdminPresenter
-{
-
+class PlatosPresenter extends BaseAdminPresenter {
+    
     /** @var $platoEditado Plato */
     private $platoEditado;
-
-    public function renderDefault(): void
-    {
+    
+    public function renderDefault(): void {
         $this->template->platos = $this->orm->platos->findAll();
     }
-
-
-    public function createComponentMasPlatosForm()
-    {
-
-        $form = (new FormFactory())->create();
-
+    
+    public function createComponentMasPlatosForm() {
+        
+        $form = ( new FormFactory() )->create();
         $form->addText('nombre', 'Nombre del plato')
-            ->setRequired();
+             ->setRequired();
         $form->addText('precio', 'Precio del plato')
-            ->setRequired()
-            ->addRule(Form::FLOAT, 'Debe ser un numero')
-            ->setHtmlAttribute("step", '.01');
-
+             ->setRequired()
+             ->addRule(Form::FLOAT, 'Debe ser un numero')
+             ->setHtmlAttribute("step", '.01');
         $form->addSubmit('send', 'Añadir')
-            ->setHtmlAttribute("class", 'btn btn-success');
-
-        $form->onSuccess[] = [$this, 'onSuccessMasPlatos'];
+             ->setHtmlAttribute("class", 'btn btn-success');
+        $form->onSuccess[] = [ $this, 'onSuccessMasPlatos' ];
+        
         return $form;
-
     }
-
-
-    public function onSuccessMasPlatos(Form $form, \stdClass $values): void
-    {
-
-
+    
+    public function onSuccessMasPlatos( Form $form, \stdClass $values ): void {
+        
+        
         try {
             $plato = new Plato();
             $plato->nombre = $values->nombre;
@@ -55,118 +43,99 @@ class PlatosPresenter extends BaseAdminPresenter
 //            $platoNuevo = $this->getPlatosModel()->newPlato($plato);
             $this->orm->persistAndFlush($plato);
             $this->flashMessage('El plato ha sido añadido a la base de datos', 'success');
-        } catch (\Exception $e) {
-            $this->flashMessage("Error: ".$e->getMessage(), 'danger');
+        } catch( \Exception $e ) {
+            $this->flashMessage("Error: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('this');
-
     }
-
-    public function actionEditar($id)
-    {
+    
+    public function actionEditar( $id ) {
         $plato = $this->orm->platos->getById($id);
         $this->platoEditado = $plato;
         //
         $this->template->item = $plato;
         $this->template->ingredientes = $plato->ingredientes; //$this->getPlatosModel()->getProductos($plato->toArray());
     }
-
-    public function createComponentEditarPlatoForm()
-    {
-
-        $form = (new PlatosFormFactory())->createEdit($this->platoEditado);
-
-        $form->onSuccess[] = [$this, 'onSuccessEditarPlato'];//convención con la variable onSuccess y el nombre del formulario
-
+    
+    public function createComponentEditarPlatoForm() {
+        
+        $form = ( new PlatosFormFactory() )->createEdit($this->platoEditado);
+        $form->onSuccess[] = [ $this, 'onSuccessEditarPlato' ];//convención con la variable onSuccess y el nombre del formulario
+        
         return $form;
     }
-
-    public function onSuccessEditarPlato(Form $form, \stdClass $values)
-    {
-
+    
+    public function onSuccessEditarPlato( Form $form, \stdClass $values ) {
+        
         try {
             $plato = $this->platoEditado;
             $plato->nombre = $values->nombre;
-
-
             $this->orm->persistAndFlush($plato);
             $this->flashMessage("Producto editado correctamente", "success");
-        } catch (\Exception $e) {
+        } catch( \Exception $e ) {
             $this->flashMessage("Error al editar", 'danger');
         }
-
         $this->redirect("this");
     }
-
+    
     public function createComponentMasIngredientesForm() {
         $plato = $this->platoEditado->id;
-        $form = (new FormFactory())->create();
-        $form->addSelect('ingrediente','Ingrediente',$this->orm->productos->findAll()->fetchPairs('id', 'nombre'));
-        $form->addText('cantidad','Cantidad')
-          ->addRule(FORM::INTEGER, 'Debe ser un numero entero')
-          ->setRequired();
+        $form = ( new FormFactory() )->create();
+        $form->addSelect('ingrediente', 'Ingrediente', $this->orm->productos->findAll()->fetchPairs('id', 'nombre'));
+        $form->addText('cantidad', 'Cantidad')
+             ->addRule(FORM::INTEGER, 'Debe ser un numero entero')
+             ->setRequired();
         $form->addSubmit('send', 'Añadir')
-          ->setHtmlAttribute("class", 'btn btn-success');
-
-        $form->onSuccess[] = [$this, 'onSuccessMasIngredientes'];
+             ->setHtmlAttribute("class", 'btn btn-success');
+        $form->onSuccess[] = [ $this, 'onSuccessMasIngredientes' ];
+        
         return $form;
     }
-
-    public function onSuccessMasIngredientes(Form $form, \stdClass $values): void
-    {
-
+    
+    public function onSuccessMasIngredientes( Form $form, \stdClass $values ): void {
+        
         try {
-
+            
             $producto = $this->orm->productos->getById($values->ingrediente);
-
+            //
             $ingrediente = new Ingrediente();
             $ingrediente->cantidad = $values->cantidad;
-
             $ingrediente->producto = $producto;
-
             $ingredienteCreado = $this->orm->persist($ingrediente);
-            
-
+            //
             $this->platoEditado->ingredientes->add($ingredienteCreado);
-
             $this->orm->persistAndFlush($this->platoEditado);
             $this->flashMessage('El plato ha sido añadido a la base de datos', 'success');
-        } catch (\Exception $e) {
-            $this->flashMessage("Error: ".$e->getMessage(), 'danger');
+        } catch( \Exception $e ) {
+            $this->flashMessage("Error: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('this');
-
     }
-    public function actionBorrar($id)
-    {
+    
+    public function actionBorrar( $id ) {
         try {
-            if (!$plato = $this->orm->platos->findById($id)) {
-                $this->flashMessage("Error al eliminar plato", "danger");
+            if( !$plato = $this->orm->platos->findById($id) ) {
+                $this->flashMessage("Plato no encontrado", "danger");
             };
             $plato->delete();
             $this->flashMessage("Plato eliminado", "success");
-        } catch (\Exception $e) {
+        } catch( \Exception $e ) {
             $this->flashMessage("Error al eliminar plato, ¿Es una id válida?: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('default');
     }
-    public function actionBorrarIngrediente($id)
-    {
+    
+    public function actionBorrarIngrediente( $id ) {
         try {
-            if (!$producto = $this->orm->ingredientes->findById($id)) {
-                $this->flashMessage("Error al eliminar Ingrediente", "danger");
+            if( !$ingrediente = $this->orm->ingredientes->findById($id) ) {
+                $this->flashMessage("Ingrediente no encontrado", "danger");
             };
-            $producto->delete();
+            $this->orm->removeAndFlush($ingrediente);
             $this->flashMessage("Producto eliminado", "success");
-        } catch (\Exception $e) {
+        } catch( \Exception $e ) {
             $this->flashMessage("Error al eliminar producto, ¿Es una id válida?: " . $e->getMessage(), 'danger');
         }
-
         $this->redirect('default');
     }
-
-
+    
 }
