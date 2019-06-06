fe879370294115a25d3c196cf2c5fce2b6196677
Gyswu <o.lugovskyy@gmail.com>
2019-06-06 17:40:07 +0200

    Solucionado el tema ultimo pedido

--- app/Presenters/PedidosPresenter.php
+++ app/Presenters/PedidosPresenter.php
@@ -40,7 +40,7 @@ final class PedidosPresenter extends BasePresenter {
             $this->redirect("Mesas:default");
         }
         //CARGA DEL ULTIMO PEDIO DE LA MESA
-        $pedidos = $this->orm->pedidos->findBy([ 'mesa' => $mesaID ])->orderBy('id', ICollection::DESC)->limitBy(2);
+        $pedidos = $this->orm->pedidos->findBy([ 'mesa' => $mesaID ])->orderBy('id', ICollection::DESC)->limitBy(1);
         foreach( $pedidos as $pedido ) {
             $id = $pedido->id;
         }
